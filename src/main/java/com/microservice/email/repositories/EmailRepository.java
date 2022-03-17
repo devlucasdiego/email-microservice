27dad9e67a82cd60a0b110d0fe58a74b10baffb1
Lucas Diego <luucasdiego_12@hotmail.com>
2022-03-17 15:48:21 -0400

    Classes iniciais

---
+++ src/main/java/com/microservice/email/repositories/EmailRepository.java
@@ -0,0 +1,8 @@
+package com.microservice.email.repositories;
+
+import com.microservice.email.models.EmailModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmailRepository extends JpaRepository<EmailModel, Long> {
+
+}
